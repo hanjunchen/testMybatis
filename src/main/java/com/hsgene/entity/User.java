b96375e02ff6f4cdf7ae6b926040a61fa7d9074c
韩俊辰 <812878994@qq.com>
2016-08-12 16:22:53 +0800

    lombok需要加入依赖

--- src/main/java/com/hsgene/entity/User.java
+++ src/main/java/com/hsgene/entity/User.java
@@ -1,10 +1,17 @@
 package com.hsgene.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
 /**
  * Created by lenovo on 2016/6/16.
  */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class User {
 
     private Integer id;
@@ -12,10 +19,6 @@ public class User {
     private String userName;
     private String userPassword;
     private String birthday;// 只要属性名和字段名相同，就算类型不一样，mybatis也会自动类型转换
-
-    public User() {
-        super();
-    }
 
     public User(Integer id) {
         this.id = id;
@@ -35,51 +38,4 @@ public class User {
         this.userPassword = userPassword;
     }
 
-    public User(Integer id, String userCode, String userName, String userPassword, String birthday) {
-        this.id = id;
-        this.userCode = userCode;
-        this.userName = userName;
-        this.userPassword = userPassword;
-        this.birthday = birthday;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getUserCode() {
-        return userCode;
-    }
-
-    public void setUserCode(String userCode) {
-        this.userCode = userCode;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public String getUserPassword() {
-        return userPassword;
-    }
-
-    public void setUserPassword(String userPassword) {
-        this.userPassword = userPassword;
-    }
-
-    public String getBirthday() {
-        return birthday;
-    }
-
-    public void setBirthday(String birthday) {
-        this.birthday = birthday;
-    }
 }
